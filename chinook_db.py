425085180a602c57752614255a048f80ba8e65d5
Anirudh <97222085+lzytitan494@users.noreply.github.com>
2024-08-16 15:10:51 +0530

    for downloading chinook database

---
+++ chinook_db.py
@@ -0,0 +1,13 @@
+import requests
+url = "https://storage.googleapis.com/benchmarks-artifacts/chinook/Chinook.db"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    # Open a local file in binary write mode
+    with open("Chinook.db", "wb") as file:
+        # Write the content of the response (the file) to the local file
+        file.write(response.content)
+    print("File downloaded and saved as Chinook.db")
+else:
+    print(f"Failed to download the file. Status code: {response.status_code}")
